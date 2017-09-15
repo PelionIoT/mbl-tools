02486a59dfd50081e37ceb841f6119449b134b77
Marcus Shawcroft <marcus.shawcroft@arm.com>
2017-09-15 11:03:04 +0100

    build-optee: Introduce run-me.sh
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>

---
+++ build-optee/run-me.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright (c) 2017 ARM Ltd.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+set -u
+
+containername="optee"
+
+workdir="build-optee"
+workdir=$(readlink -f "$workdir")
+
+docker build -t "$containername" ./mbl-tools/build-optee/
+
+mkdir -p "$workdir"
+
+docker run --rm -t -i \
+       -v "$workdir":/optee "$containername" \
+       ./build.sh --builddir /optee "$@"
