1d07c0420d27c00b39f011e0995f9875d3504883
Marcus Shawcroft <marcus.shawcroft@arm.com>
2017-09-18 12:27:51 +0100

    build-mbl: Add framework for option handling in run-me.sh
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>

--- build-mbl/run-me.sh
+++ build-mbl/run-me.sh
@@ -7,6 +7,50 @@
 set -e
 set -u
 
+usage()
+{
+  cat <<EOF
+
+usage: run-me.sh [OPTION] -- [build.sh arguments]
+
+  -h, --help            Print brief usage information and exit.
+  -x                    Enable shell debugging in this script.
+
+EOF
+}
+
+args=$(getopt -o+hx -l help -n "$(basename "$0")" -- "$@")
+eval set -- "$args"
+while [ $# -gt 0 ]; do
+  if [ -n "${opt_prev:-}" ]; then
+    eval "$opt_prev=\$1"
+    opt_prev=
+    shift 1
+    continue
+  elif [ -n "${opt_append:-}" ]; then
+    eval "$opt_append=\"\${$opt_append:-} \$1\""
+    opt_append=
+    shift 1
+    continue
+  fi
+  case $1 in
+  -h | --help)
+    usage
+    exit 0
+    ;;
+
+  -x)
+    set -x
+    ;;
+
+  --)
+    shift
+    break 2
+    ;;
+  esac
+  shift 1
+done
+
 containername="mbl-manifest-env"
 
 workdir="build-mbl-manifest"
