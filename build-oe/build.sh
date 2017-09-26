e647f1300cc130c7ad8837b25aebad6461d528d4
Marcus Shawcroft <marcus.shawcroft@arm.com>
2017-09-26 12:54:33 +0100

    Add missing shell quoting.
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>

--- build-oe/build.sh
+++ build-oe/build.sh
@@ -61,7 +61,7 @@ Useful STAGE names:
 
 EOF
 }
-args=$(getopt -o+hx -l builddir:,clean,no-clean,help,target: -n $(basename "$0") -- "$@")
+args=$(getopt -o+hx -l builddir:,clean,no-clean,help,target: -n "$(basename "$0")" -- "$@")
 eval set -- "$args"
 while [ $# -gt 0 ]; do
   if [ -n "${opt_prev:-}" ]; then
