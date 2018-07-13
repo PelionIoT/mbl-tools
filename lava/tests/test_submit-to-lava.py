363c1726e53de76b66267e0d1a47dac40457425e
Diego Russo <diego.russo@arm.com>
2018-07-13 14:00:46 +0100

    Add pytest requirement and create test skeleton
    Signed-off-by: Diego Russo <diego.russo@arm.com>

---
+++ lava/tests/test_submit-to-lava.py
@@ -0,0 +1,45 @@
+class TestLAVATemplates(object):
+    def test___init__(self):
+        assert False
+
+    def test_process(self):
+        assert False
+
+    def test__dump_job(self):
+        assert False
+
+    def test__load_template(self):
+        assert False
+
+
+class TestLAVAServer(object):
+    def test___init__(self):
+        assert False
+
+    def test_submit_job(self):
+        assert False
+
+    def test_get_job_urls(self):
+        assert False
+
+    def test__connect(self):
+        assert False
+
+    def test__normalise_url(self):
+        assert False
+
+    def test__get_api_url(self):
+        assert False
+
+    def test__get_job_info_url(self):
+        assert False
+
+class TestParseArguments(object):
+    def test__parse_arguments(self):
+        assert False
+
+def test__enable_debug_logging():
+    assert False
+
+def test__main():
+    assert False
