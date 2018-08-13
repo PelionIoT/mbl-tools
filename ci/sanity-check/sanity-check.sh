f2984f5c03ed59438f4d5e1a7ee15dafa88a9804
Diego Russo <diego.russo@arm.com>
2018-08-13 16:27:19 +0100

    Replace cho with printf
    Signed-off-by: Diego Russo <diego.russo@arm.com>

--- ci/sanity-check/sanity-check.sh
+++ ci/sanity-check/sanity-check.sh
@@ -70,31 +70,31 @@ workdir=$(readlink -f "$workdir")
 # Collect all shell files
 SHELL_FILES=$(find "$workdir" -type f | \
               while read -r in; do if file -i "${in}" | \
-              grep -q text/x-shellscript; then echo "${in}"; fi; done)
+              grep -q text/x-shellscript; then printf "%s\n" "${in}"; fi; done)
 
 # Custom script to check tabs existence in shell scripts
 printf "Running tab_finder.py on shell files...\n"
-echo "$SHELL_FILES" | xargs --no-run-if-empty "$execdir/tab_finder.py" || rc=1
+printf "%s" "$SHELL_FILES" | xargs --no-run-if-empty "$execdir/tab_finder.py" || rc=1
 
 # Run shellcheck on bash files
 printf "Running shellcheck on shell files...\n"
-echo "$SHELL_FILES" | xargs --no-run-if-empty shellcheck --format=gcc  || rc=1
+printf "%s" "$SHELL_FILES" | xargs --no-run-if-empty shellcheck --format=gcc  || rc=1
 
 # Collect all Python files
 PYTHON_FILES=$(find "$workdir" -type f | \
                while read -r in; do if file -i "${in}" | \
-               grep -q x-python; then echo "${in}"; fi; done)
+               grep -q x-python; then printf "%s\n" "${in}"; fi; done)
 
 # Run black on python files - https://black.readthedocs.io/en/stable/
 printf "Running black on python files...\n"
-echo "$PYTHON_FILES" | xargs --no-run-if-empty black -v --check --diff || rc=1
+printf "%s" "$PYTHON_FILES" | xargs --no-run-if-empty black -v --check --diff || rc=1
 
 # Run pycodestyle on python files
 printf "Running pycodestyle on python files...\n"
-echo "$PYTHON_FILES" | xargs --no-run-if-empty pycodestyle || rc=1
+printf "%s" "$PYTHON_FILES" | xargs --no-run-if-empty pycodestyle || rc=1
 
 # Run pep257 on python files
 printf "Running pep257 on python files...\n"
-echo "$PYTHON_FILES" | xargs --no-run-if-empty pep257 || rc=1
+printf "%s" "$PYTHON_FILES" | xargs --no-run-if-empty pep257 || rc=1
 
 exit "$rc"
