3c53943de3d52d49d15bfb85c1f15c8fb9127b4a
Diego Russo <diego.russo@arm.com>
2018-08-15 14:55:38 +0100

    Implement fixes to pass sanity check on lava directory
    Signed-off-by: Diego Russo <diego.russo@arm.com>

--- lava/submit-to-lava.py
+++ lava/submit-to-lava.py
@@ -4,6 +4,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+"""Submit to LAVA application.
+
+This application is responsible to submit jobs to lava
+"""
 import argparse
 import logging
 import sys
@@ -20,24 +24,30 @@ default_template_name = "template.yaml"
 
 class LAVATemplates(object):
     """LAVA templates class."""
-    def __init__(self, template_path, device_type, lava_template_names,
-                 dry_run):
+
+    def __init__(
+        self, template_path, device_type, lava_template_names, dry_run
+    ):
+        """Initialise LAVATemplates class."""
         self.template_path = template_path
         self.device_type = device_type
         self.lava_template_names = lava_template_names
         self.dry_run = dry_run
 
-    def process(self, image_url, build_tag, build_url, notify_user,
-                notify_emails):
+    def process(
+        self, image_url, build_tag, build_url, notify_user, notify_emails
+    ):
         """Process templates rendering them with the right values."""
         lava_jobs = []
         for template_name in self.lava_template_names:
             template = self._load_template(template_name)
-            lava_job = template.render(image_url=image_url,
-                                       build_tag=build_tag,
-                                       build_url=build_url,
-                                       notify_user=notify_user,
-                                       notify_emails=notify_emails)
+            lava_job = template.render(
+                image_url=image_url,
+                build_tag=build_tag,
+                build_url=build_url,
+                notify_user=notify_user,
+                notify_emails=notify_emails,
+            )
             lava_jobs.append(lava_job)
             if self.dry_run:
                 self._dump_job(lava_job, template_name)
@@ -57,15 +67,20 @@ class LAVATemplates(object):
         """Return a jinja2 template starting from a yaml file on disk."""
         try:
             if template_name:
-                template_full_path = os.path.join(self.template_path,
-                                                  self.device_type)
+                template_full_path = os.path.join(
+                    self.template_path, self.device_type
+                )
                 template_loader = jinja2.FileSystemLoader(
-                    searchpath=template_full_path)
+                    searchpath=template_full_path
+                )
                 template_env = jinja2.Environment(loader=template_loader)
                 template = template_env.get_template(template_name)
         except jinja2.exceptions.TemplateNotFound as e:
-            raise Exception("Cannot find template {} on {}"
-                            .format(template_name, template_full_path))
+            raise Exception(
+                "Cannot find template {} on {}".format(
+                    template_name, template_full_path
+                )
+            )
         return template
 
 
@@ -73,6 +88,7 @@ class LAVAServer(object):
     """LAVA server class."""
 
     def __init__(self, server_url, username, token, dry_run):
+        """Initialise LAVAServer class."""
         self.base_url = self._normalise_url(server_url)
         self.api_url = self._get_api_url(username, token)
         self.job_info_url = self._get_job_info_url()
@@ -117,8 +133,10 @@ class LAVAServer(object):
 
     def _normalise_url(self, server_url):
         """Return LAVA base url."""
-        if not (server_url.startswith("http://") or
-                server_url.startswith("https://")):
+        if not (
+            server_url.startswith("http://")
+            or server_url.startswith("https://")
+        ):
             server_url = "https://{}".format(server_url)
         logging.debug("Base LAVA url: {}".format(server_url))
         return server_url
@@ -126,84 +144,104 @@ class LAVAServer(object):
     def _get_api_url(self, username, token):
         """Return LAVA API url."""
         url = urllib.parse.urlsplit(self.base_url)
-        api_url = "{}://{}:{}@{}/RPC2".format(url.scheme, username, token,
-                                              url.netloc)
+        api_url = "{}://{}:{}@{}/RPC2".format(
+            url.scheme, username, token, url.netloc
+        )
         logging.debug("API LAVA url: {}".format(api_url))
         return api_url
 
     def _get_job_info_url(self):
         """Return LAVA base url for job details."""
         url = urllib.parse.urlsplit(self.base_url)
-        job_info_url = "{}://{}/scheduler/job/{{}}".format(url.scheme,
-                                                           url.netloc)
+        job_info_url = "{}://{}/scheduler/job/{{}}".format(
+            url.scheme, url.netloc
+        )
         logging.debug("Job info LAVA url: {}".format(job_info_url))
         return job_info_url
 
 
 def _parse_arguments(cli_args):
-    """Arguments parser."""
+    """Parse arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--lava-server",
-                        help="LAVA server URL",
-                        dest="lava_server",
-                        required=True)
-    parser.add_argument("--lava-username",
-                        help="LAVA username",
-                        dest="lava_username",
-                        required=True)
-    parser.add_argument("--lava-token",
-                        help="LAVA token for API",
-                        dest="lava_token",
-                        required=True)
-    parser.add_argument("--device-type",
-                        help="Device type in LAVA",
-                        dest="device_type",
-                        required=True)
-    parser.add_argument("--image-url",
-                        help="Image to flash onto the device",
-                        dest="image_url",
-                        required=True)
-    parser.add_argument("--build-tag",
-                        help="Build tag",
-                        dest="build_tag")
-    parser.add_argument("--build-url",
-                        help="Build url",
-                        dest="build_url")
-    parser.add_argument("--template-path",
-                        help="Path to LAVA job templates",
-                        dest="template_path",
-                        default=default_template_base_path)
-    parser.add_argument("--template-names",
-                        help="list of the templates to submit for testing",
-                        dest="template_names",
-                        nargs="+",
-                        default=[default_template_name])
-    parser.add_argument("--notify-user",
-                        help="Enable email notification to the user",
-                        action="store_true",
-                        dest="notify_user",
-                        default=False)
-    parser.add_argument("--notify-emails",
-                        help="Enable email notification to custom emails",
-                        dest="notify_emails",
-                        nargs="+",
-                        default=[])
-    parser.add_argument("--debug",
-                        help="Enable debug messages",
-                        action="store_true",
-                        dest="debug",
-                        default=False)
-    parser.add_argument("--dry-run",
-                        help="""Prepare and write templates to tmp/.
+    parser.add_argument(
+        "--lava-server",
+        help="LAVA server URL",
+        dest="lava_server",
+        required=True,
+    )
+    parser.add_argument(
+        "--lava-username",
+        help="LAVA username",
+        dest="lava_username",
+        required=True,
+    )
+    parser.add_argument(
+        "--lava-token",
+        help="LAVA token for API",
+        dest="lava_token",
+        required=True,
+    )
+    parser.add_argument(
+        "--device-type",
+        help="Device type in LAVA",
+        dest="device_type",
+        required=True,
+    )
+    parser.add_argument(
+        "--image-url",
+        help="Image to flash onto the device",
+        dest="image_url",
+        required=True,
+    )
+    parser.add_argument("--build-tag", help="Build tag", dest="build_tag")
+    parser.add_argument("--build-url", help="Build url", dest="build_url")
+    parser.add_argument(
+        "--template-path",
+        help="Path to LAVA job templates",
+        dest="template_path",
+        default=default_template_base_path,
+    )
+    parser.add_argument(
+        "--template-names",
+        help="list of the templates to submit for testing",
+        dest="template_names",
+        nargs="+",
+        default=[default_template_name],
+    )
+    parser.add_argument(
+        "--notify-user",
+        help="Enable email notification to the user",
+        action="store_true",
+        dest="notify_user",
+        default=False,
+    )
+    parser.add_argument(
+        "--notify-emails",
+        help="Enable email notification to custom emails",
+        dest="notify_emails",
+        nargs="+",
+        default=[],
+    )
+    parser.add_argument(
+        "--debug",
+        help="Enable debug messages",
+        action="store_true",
+        dest="debug",
+        default=False,
+    )
+    parser.add_argument(
+        "--dry-run",
+        help="""Prepare and write templates to tmp/.
                         Don"t submit to actual servers.""",
-                        action="store_true",
-                        dest="dry_run")
+        action="store_true",
+        dest="dry_run",
+    )
 
     return parser.parse_args(cli_args)
 
 
 def _set_default_args(args):
-    """Set default arguments """
+    """Set default arguments."""
     # Set default build tag name based on lava username
     default_build_tag = "{} build".format(args.lava_username)
     args.build_tag = args.build_tag if args.build_tag else default_build_tag
@@ -244,17 +282,26 @@ def _main(args):
         args = _set_default_args(args)
 
         # Load LAVA templates
-        lava_template = LAVATemplates(args.template_path, args.device_type,
-                                      args.template_names, args.dry_run)
+        lava_template = LAVATemplates(
+            args.template_path,
+            args.device_type,
+            args.template_names,
+            args.dry_run,
+        )
 
         # Create LAVA jobs yaml file from templates
-        lava_jobs = lava_template.process(args.image_url, args.build_tag,
-                                          args.build_url, args.notify_user,
-                                          args.notify_emails)
+        lava_jobs = lava_template.process(
+            args.image_url,
+            args.build_tag,
+            args.build_url,
+            args.notify_user,
+            args.notify_emails,
+        )
 
         # Instantiate a LAVA server
-        lava_server = LAVAServer(args.lava_server, args.lava_username,
-                                 args.lava_token, args.dry_run)
+        lava_server = LAVAServer(
+            args.lava_server, args.lava_username, args.lava_token, args.dry_run
+        )
 
         for lava_job in lava_jobs:
             # Submit the job to LAVA
@@ -269,6 +316,7 @@ def _main(args):
         return 1
     except Exception as e:
         import traceback
+
         logging.error(e)
         traceback.print_exc()
         return 2
