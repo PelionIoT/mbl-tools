dcbafed608595da963b0c14d185232fa304ef999
Jeremy Johnson <jeremy.johnson@arm.com>
2018-11-09 15:55:20 +0000

    IOTMBL-637: Updated license and copyright information (#98)
    Updated licenses and source code to meet open source license
        compliance process.

--- ci/sanity-check/sanity-check.sh
+++ ci/sanity-check/sanity-check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Copyright (c) 2018 ARM Ltd.
+# Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
 #
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: BSD-3-Clause
 
 set -e
 set -u
