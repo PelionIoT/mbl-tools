23753e7e3e0da9f9151a726dbbea3e8448de60f7
Amir BBen Avraham <amir.benavraham@arm.com>
2018-12-20 08:07:33 +0200

    Rename mbl-console-image to mbl-image-production, and mbl-console-image-test to mbl-image-development
    Update readme doc
    
    Task: IOTMBL-522

--- build-mbl/build.sh
+++ build-mbl/build.sh
@@ -96,7 +96,7 @@ default_manifest="default.xml"
 default_url="git@github.com:ARMmbed/mbl-manifest.git"
 default_machines="raspberrypi3-mbl"
 default_distro="mbl"
-default_images="mbl-image-release mbl-image-developer"
+default_images="mbl-image-production mbl-image-development"
 
 # Set of license package name (PN) exceptions
 # This hash array uses a key (PN) created from reading the recipeinfo
@@ -112,7 +112,7 @@ license_package_exceptions=(
   ["go-cross-arm"]="go-cross"
   ["kmod"]="kmod_git"
   ["libtool"]="libtool-native"
-  ["mbl-image-release"]="mbl-image-release.bb"
+  ["mbl-image-production"]="mbl-image-production.bb"
   ["ncurses"]="ncurses/ncurses"
   ["packagegroup-mbl"]="packagegroup-mbl.bb")
 
