fd4101016711526cc92471a3bc6c1672d2884456
Robert Walton <rob.walton@arm.com>
2019-01-10 22:01:20 +0000

    To be squashed: sanity checks

--- ci/launch-container/lib/args.py
+++ ci/launch-container/lib/args.py
@@ -4,6 +4,8 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+"""Argument parser for launch_docker_container."""
+
 import argparse
 import os
 
@@ -12,33 +14,24 @@ def parse_args():
     """Parse cli arguments."""
     parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        "image",
-        help="Name of the image to create."
-    )
+    parser.add_argument("image", help="Name of the image to create.")
+
+    parser.add_argument("container", help="Name of the container to create.")
+
+    parser.add_argument("dockerfile", help="Path to the Dockerfile.")
 
     parser.add_argument(
-        "container",
-        help="Name of the container to create."
-    )
-
-    parser.add_argument(
-        "dockerfile",
-        help="Path to the Dockerfile."
-    )
-
-    parser.add_argument(
-        '-w',
-        '--workdir',
+        "-w",
+        "--workdir",
         default=os.getcwd(),
-        help="The working directory to add to the container."
+        help="The working directory to add to the container.",
     )
 
     parser.add_argument(
         "--cp",
-        help="Copy files from the container. " 
+        help="Copy files from the container. "
         "Requires a path (inside the container) to copy any artifacts from. "
-        "Artifacts will be copied to --workdir."
+        "Artifacts will be copied to --workdir.",
     )
 
     return parser.parse_args()
