75fd767b3fe1bae9165ba83a74484ea1a947238a
Hugues Kamba <hugues.kamba@arm.com>
2019-01-18 22:42:15 +0000

    Add shell scripts to run licensing checks
    For IOTMBL-1414 Create a MBL licensing Jenkins shared library
    
    * Add run-me.sh to run checks in a Docker container.
    * Add licensing-check.sh to run checks natively.

---
+++ ci/licensing-check/README.md
@@ -0,0 +1,40 @@
+<!-- Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+
+SPDX-License-Identifier: BSD-3-Clause -->
+# What licensing checks are running?
+
+The [`mbl-licensing-checker`] python application runs to check for expected license and copyright notice on first and third party source files.
+
+The sanity check script fails if the application returns a non-zero exit code.
+
+## Requirements
+
+The easiest way is to run it through Docker, hence the only requirement is to have Docker installed.
+
+To run it natively, install the [`mbl-licensing-checker`] application.
+
+## Usage
+
+### Running with Docker
+
+```
+$ ./run-me.sh --workdir /path/to/file/or/directory
+```
+
+The script runs the [`mbl-licensing-checker`] application within a Docker container which contains all required dependencies.
+The output will be printed on the screen with the result.
+
+### Running natively
+
+```
+$ ./licensing-check.sh --workdir /path/to/file/or/directory
+```
+
+The script runs the [`mbl-licensing-checker`] application natively.
+The output will be printed on the screen with the result.
+
+### Skipping directories
+
+You can disable sanity check for a specific sub-tree by placing an [`mbl-licensing-checker`] configuration file with the convention set to none. See [here](../../mbl-licensing-checker/README.md) for more information.
+
+[`mbl-licensing-checker`]: ../../mbl-licensing-checker
---
+++ ci/licensing-check/run-me.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+set -e
+set -u
+
+execdir="$(readlink -e "$(dirname "$0")")"
+
+default_imagename="mbl-licensing-check-env"
+default_containername="mbl-licensing-check-container.$$"
+
+trap cleanup 0
+
+cleanup() {
+    # This command will return an id (eg. 43008e2a9f5a) of the running
+    # container
+    running_container="$(docker ps -q -f name="$default_containername")"
+    if [ ! -z "$running_container" ]; then
+        docker kill "$default_containername"
+    fi
+}
+
+usage()
+{
+  cat <<EOF
+
+usage: run-me.sh [OPTION] -- [run-me.sh arguments]
+
+  -h, --help            Print brief usage information and exit.
+  --image-name NAME     Specify the docker image name to build. Default ${default_imagename}.
+  --tty                 Enable tty creation (default).
+  --no-tty              Disable tty creation.
+  --workdir PATH        Specify the directory to check.  Default PWD.
+  -x                    Enable shell debugging in this script.
+
+EOF
+}
+
+imagename="$default_imagename"
+flag_tty="-t"
+
+args=$(getopt -o+hx -l help,image-name:,tty,no-tty,workdir: -n "$(basename "$0")" -- "$@")
+eval set -- "$args"
+while [ $# -gt 0 ]; do
+  if [ -n "${opt_prev:-}" ]; then
+    eval "$opt_prev=\$1"
+    opt_prev=
+    shift 1
+    continue
+  elif [ -n "${opt_append:-}" ]; then
+    eval "$opt_append=\"\${$opt_append:-} \$1\""
+    opt_append=
+    shift 1
+    continue
+  fi
+  case $1 in
+  -h | --help)
+    usage
+    exit 0
+    ;;
+
+  --image-name)
+    opt_prev=imagename
+    ;;
+
+  --tty)
+    flag_tty="-t"
+    ;;
+
+  --no-tty)
+    flag_tty=
+    ;;
+
+  --workdir)
+    opt_prev=workdir
+    ;;
+
+  -x)
+    set -x
+    ;;
+
+  --)
+    shift
+    break 2
+    ;;
+  esac
+  shift 1
+done
+
+if [ -z "${workdir:-}" ]; then
+  workdir="$(pwd)"
+fi
+workdir=$(readlink -f "$workdir")
+
+cp -r ../../mbl-licensing-checker ./
+
+docker build -t "$imagename" "$execdir"
+
+rm -rf mbl-licensing-checker
+
+docker run --rm -i $flag_tty \
+       --name "$default_containername" \
+       -e LOCAL_UID="$(id -u)" -e LOCAL_GID="$(id -g)" \
+       -v "$workdir":/work "$imagename" \
+       ./licensing-check.sh --workdir /work \
+       "$@"
+
