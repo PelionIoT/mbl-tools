003f0bf9ac625aee31e366150116f4ce5397abdc
Diego Russo <diego.russo@arm.com>
2019-01-23 10:21:57 +0000

    Modifying licence

--- build-mbl/build.sh
+++ build-mbl/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
-#
+# Copyright (c) 2018, diegor Arm Limited and Contributors. All rights reserved.
+# 
 # SPDX-License-Identifier: BSD-3-Clause
 
 # Workspace layout:
