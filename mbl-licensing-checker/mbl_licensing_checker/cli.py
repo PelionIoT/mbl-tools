95888c2ac5798eece8c08fee6c88d09a4414a3e6
Hugues Kamba <hugues.kamba@arm.com>
2019-01-28 13:57:02 +0000

    Update cli.py

--- mbl-licensing-checker/mbl_licensing_checker/cli.py
+++ mbl-licensing-checker/mbl_licensing_checker/cli.py
@@ -5,7 +5,7 @@
 # Modifications: Copyright (c) 2019 Arm Limited and Contributors. All rights
 #                reserved.
 #
-# SPDX-License-Idenvtifier: MIT
+# SPDX-License-Idgenvtifier: MIT
 """Command line interface for mbl-licensing-checker."""
 import logging
 import sys
