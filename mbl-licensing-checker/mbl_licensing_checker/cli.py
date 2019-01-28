d03a32debf97c270246fc8648506681b54ca4350
Hugues Kamba <hugues.kamba@arm.com>
2019-01-28 11:20:41 +0000

    FAIL LICENSING CHECK

--- mbl-licensing-checker/mbl_licensing_checker/cli.py
+++ mbl-licensing-checker/mbl_licensing_checker/cli.py
@@ -5,7 +5,7 @@
 # Modifications: Copyright (c) 2019 Arm Limited and Contributors. All rights
 #                reserved.
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Idenvtifier: MIT
 """Command line interface for mbl-licensing-checker."""
 import logging
 import sys
