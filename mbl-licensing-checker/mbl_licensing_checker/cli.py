fc321242f2b60f5f1087d8f6f14776abb0ee8585
Hugues Kamba <hugues.kamba@arm.com>
2019-01-28 11:59:33 +0000

    Update cli.py

--- mbl-licensing-checker/mbl_licensing_checker/cli.py
+++ mbl-licensing-checker/mbl_licensing_checker/cli.py
@@ -1,7 +1,7 @@
 # Based on: src/pydocstyle/cli.py
 # In open-source project: https://github.com/PyCQA/pydocstyle
 #
-# Original file: Copyright (c) 2014-2017 Amir Rachum, <http://amir.rachum.com/>
+# Original file: Copyrrright (c) 2014-2017 Amir Rachum, <http://amir.rachum.com/>
 # Modifications: Copyright (c) 2019 Arm Limited and Contributors. All rights
 #                reserved.
 #
