3b22d2adfb9f4db8404b926cdd0176bb4af85605
Jeremy Johnson <jeremy.johnson@arm.com>
2019-07-01 12:39:15 +0100

    TO BE SQUAHED: Minor improvements to git-sync-tag script

--- maintenance/scripts/git-sync-tag.bash
+++ maintenance/scripts/git-sync-tag.bash
@@ -4,23 +4,27 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+REMOTE=$(git remote)
+
+MASTER=master
+# Work out default development head
+HEAD=$(git remote show "$REMOTE" | grep "HEAD branch")
+HEAD=${HEAD#*: }    # Remove spaces
+
 if [ "$1" == "-h" ]; then
     echo "Create synchonization tags on the given branches with these assumptions:"
     echo " * run in the repository checkout you want to tag;"
-    echo " * the BRANCH_FROM local head is at the point to tag (i.e. the last commit synced);"
-    echo " * the BRANCH_TO remote head is at the point to tag (i.e. last commit is the sync)."
+    echo " * BRANCH_FROM local head is at point to tag (i.e. local commit synced);"
+    echo " * BRANCH_TO remote head is at point to tag (i.e. remote commit is sync)."
     echo "Usage: $(basename "$0") [BRANCH_FROM] [BRANCH_TO]"
+    echo "Defaults: BRANCH_FROM=$HEAD / BRANCH_TO=$MASTER"
     exit 0
 fi
 
-REMOTE=$(git remote)
-
 DEV=${1}
-MST=${2-master}
+MST=${2-$MASTER}
 if [ "$DEV" == "" ]; then
-    # Work out default head
-    HEAD=$(git remote show "$REMOTE" | grep "HEAD branch")
-    DEV=${HEAD#*: }
+    DEV=$HEAD
 fi
 
 echo "BRANCH_FROM=$DEV / BRANCH_TO=$MST"
@@ -28,7 +32,7 @@ echo "BRANCH_FROM=$DEV / BRANCH_TO=$MST"
 TAG_TO=${DEV}_to_${MST}
 TAG_INTO=${DEV}_into_${MST}
 
-set -x 
+set -xe
 
 # Sync master with remote
 git fetch "$REMOTE"
