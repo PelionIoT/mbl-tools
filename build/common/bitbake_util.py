d42afb6abe63d39574348df7ee963cc370f383ab
Diego Russo <diego.russo@arm.com>
2019-09-17 16:56:16 +0100

    TO BE SQUASHED: address PR comments

--- build/common/bitbake_util.py
+++ build/common/bitbake_util.py
@@ -6,13 +6,19 @@
 
 import sys
 from pathlib import Path
-from shutil import copy2, copytree
+from shlex import quote
 
 from pexpect.replwrap import bash
 
 
-class BitbakeInvalidEnvironment(Exception):
-    """Class for errors where an invalid environment is found."""
+class BitbakeError(Exception):
+    """Base class for errors relating to the Bitbake environment."""
+
+    pass
+
+
+class BitbakeInvalidDirectoryError(BitbakeError):
+    """Class for errors where an invalid directory is found."""
 
     def __init__(self, obj):
         """
@@ -22,7 +28,24 @@ class BitbakeInvalidEnvironment(Exception):
         * obj (PathLike): the path that doesn't exist or invalid
 
         """
-        super().__init__("{} doesn't exist or invalid.".format(obj))
+        super().__init__(
+            "Directory {} doesn't exist or invalid."
+            "Please specify a valid build Bitbake directory".format(obj)
+        )
+
+
+class BitbakeInvalidFileError(BitbakeError):
+    """Class for errors where an invalid file is found."""
+
+    def __init__(self, obj):
+        """
+        Initialize an object of this class.
+
+        Args:
+        * obj (PathLike): the path that doesn't exist or invalid
+
+        """
+        super().__init__("File {} doesn't exist or invalid.".format(obj))
 
 
 class Bitbake(object):
@@ -31,7 +54,6 @@ class Bitbake(object):
     def __init__(
         self,
         builddir,
-        outputdir,
         machine,
         distro,
         init_env_file="setup-environment",
@@ -41,8 +63,7 @@ class Bitbake(object):
         Initialize an object of this class.
 
         Args:
-        * builddir (str): base Bitbake build directory
-        * outputdir (str): directory where to save artifacts
+        * builddir (PathLike): base Bitbake build directory
         * machine (str): Bitbake machine
         * distro (str): Bitbake distribution
         * init_env_file (str): initialization environment file
@@ -50,22 +71,28 @@ class Bitbake(object):
                                 variables to pass to the Bitbake init file
 
         """
-        self.builddir = Path(builddir)
-        self.outputdir = Path(outputdir)
+        self.builddir = builddir
         self.machine = machine
         self.distro = distro
         self.init_env_file = init_env_file
         self.env_variables = env_variables
-        self.shell = bash()
+        self._shell = bash()
+        self._validate_parameters()
         self._check_environment()
 
     def setup_environment(self):
         """Set up the Bitbake environment."""
-        self.run_command("cd {}".format(self.builddir))
+        self.run_command("cd {}".format(quote(str(self.builddir))))
         env_vars = self._build_env_variables_string()
+        if env_vars:
+            env_vars = quote(env_vars)
         command = "{} MACHINE={} . {} build-{}".format(
-            env_vars, self.machine, self.init_env_file, self.distro
+            env_vars,
+            quote(self.machine),
+            quote(self.init_env_file),
+            quote(self.distro),
         )
+        print(command)
         self.run_command(command)
 
     def run_command(self, command, timeout=None, stdout=True):
@@ -84,7 +111,9 @@ class Bitbake(object):
         * output (str): output of the specified command
 
         """
-        output = self.shell.run_command(command, timeout=timeout, async_=False)
+        output = self._shell.run_command(
+            command, timeout=timeout, async_=False
+        )
         if stdout:
             print(output)
         return output
@@ -108,51 +137,29 @@ class Bitbake(object):
         for command in commands:
             self.run_command(command, timeout=timeout, stdout=stdout)
 
-    def save_artifacts(self, *files):
-        """
-        Copy *files into self.outputdir directory.
+    def _build_env_variables_string(self):
+        env_string = ""
+        if self.env_variables is not None:
+            env_string = " ".join(
+                "{}={}".format(key, value)
+                for key, value in self.env_variables.items()
+            )
+        return env_string
 
-        Mandatory args:
-        * files (str arguments): the list specifies files/directory to be
-                                 copied into self.output directory
-
-
-        If a file/directory is not found, it will be skipped and it continues
-        to copy the next ones.
-
-        """
-        for obj in files:
-            obj_path = self.builddir / obj
-            if obj_path.exists():
-                if obj_path.is_file():
-                    copy2(obj_path, self.outputdir)
-                elif obj_path.is_dir():
-                    copytree(obj_path, self.outputdir / obj_path.name)
-                else:
-                    print(
-                        "{}: file type not recognised. Skipping.".format(
-                            obj_path
-                        ),
-                        file=sys.stderr,
-                    )
-            else:
-                print("{} doesn't exist. Skipping.".format(obj_path))
-
-    # Private methods
     def _check_environment(self):
         repo_dir = self.builddir / ".repo"
         if not repo_dir.exists() or not repo_dir.is_dir():
-            raise BitbakeInvalidEnvironment(repo_dir)
-        if not self.outputdir.exists() or not self.outputdir.is_dir():
-            raise BitbakeInvalidEnvironment(self.outputdir)
+            raise BitbakeInvalidDirectoryError(repo_dir)
         init_env_file_path = self.builddir / self.init_env_file
         if not init_env_file_path.exists() or not init_env_file_path.is_file():
-            raise BitbakeInvalidEnvironment(init_env_file_path)
+            raise BitbakeInvalidFileError(init_env_file_path)
 
-    def _build_env_variables_string(self):
-        string = ""
-        if isinstance(self.env_variables, dict):
-            str = " ".join(
-                "{}={}".format(key, value) for key, value in self.env_variables
-            )
-        return string
+    def _validate_parameters(self):
+        # mandatory parameters
+        assert isinstance(self.builddir, Path) and self.builddir
+        assert isinstance(self.machine, str) and self.machine
+        assert isinstance(self.distro, str) and self.distro
+        # init_env_file should be always specified
+        assert isinstance(self.init_env_file, str) and self.init_env_file
+        # env_variables is optional
+        assert isinstance(self.env_variables, dict)
