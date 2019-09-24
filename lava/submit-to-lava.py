4f113faada95a3b367a375ce7276015218dee1b7
Diego Russo <diego.russo@arm.com>
2019-09-24 17:12:40 +0100

    Address review comment

--- lava/submit-to-lava.py
+++ lava/submit-to-lava.py
@@ -215,9 +215,7 @@ class StoreDictKeyPair(argparse.Action):
     def __init__(self, option_strings, dest, nargs=None, **kwargs):
         """Initialise the class."""
         self._nargs = nargs
-        super(StoreDictKeyPair, self).__init__(
-            option_strings, dest, nargs=nargs, **kwargs
-        )
+        super().__init__(option_strings, dest, nargs=nargs, **kwargs)
 
     def __call__(self, parser, namespace, values, option_string=None):
         """Store data into the namespace."""
