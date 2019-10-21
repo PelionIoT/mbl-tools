625c938fcd41d7eac8f696578247e42c2234a9fa
Jeremy Johnson <jeremy.johnson@arm.com>
2019-10-21 16:45:28 +0100

    SQUASH: git-sync-diff update

--- maintenance/scripts/git-sync-diff.bash
+++ maintenance/scripts/git-sync-diff.bash
@@ -4,14 +4,38 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-if [ "$1" == "-h" ]; then
+usage()
+{
     echo "Review the outstanding commits from the current branch synchonization"
     echo "Outputs the list of commits in oldest first order"
     echo "Note: requires branches to have been properly tagged via git-sync-tag.bash"
     echo "Note: must use remote (e.g. origin) in branches"
-    echo "Usage: $(basename "$0") [REMOTE_BRANCH_FROM] [REMOTE_BRANCH_TO]"
+    echo "Note: use --no-tag to review more commits, in case of bad maintenance tags"
+    echo "Note: use --no-date to not show dates of the commits"
+    echo "Usage: $(basename "$0") [--no-tag] [--no-date] [REMOTE_BRANCH_FROM] [REMOTE_BRANCH_TO]"
     exit 0
-fi
+}
+
+NO_TAG=0
+COMMIT_DATE=1
+while [ $# -gt 0 ]; do
+    case $1 in
+    --no-tag)
+        NO_TAG=1
+        ;;
+    --no-date)
+        COMMIT_DATE=0
+        ;;
+    --help|-h)
+        usage
+        ;;
+    *)
+        break
+        ;;
+    esac
+    shift
+done
+
 
 REMOTE=$(git remote)
 
@@ -24,27 +48,52 @@ if [ "$DEV" == "" ]; then
 fi
 
 # Print out repo name and settings
-basename "$(git remote get-url "$REMOTE")" .git
+echo "REPO: $(basename "$(git remote get-url "$REMOTE")" .git)"
 echo "BRANCH_FROM=$DEV / BRANCH_TO=$MST"
 
 TAG_TO=$(basename "$DEV")_to_$(basename "$MST")
 
+DATE_COL="\e[36m"
+WARN_COL="\e[31m"
 DEV_COL="\e[32m"
 MST_COL="\e[33m"
 NO_COL="\e[0m"
 
-DIFF=$(git cherry -v --abbrev=7 "$MST" "$DEV" "$TAG_TO")
+if [ $NO_TAG -eq 1 ]; then
+    echo "INFO: NOT stopping at tag $TAG_TO"
+    DIFF=$(git cherry -v --abbrev=7 "$MST" "$DEV")
+else
+    # Get the diff using the tag, but double-check it seems good!
+    ALL_COMMITS=$(git cherry -v --abbrev=7 "$MST" "$DEV" | awk '$0 ~ "^+" {count++} END {print count}')
+    DIFF=$(git cherry -v --abbrev=7 "$MST" "$DEV" "$TAG_TO")
+    TAG_COMMITS=$(printf "%s" "$DIFF" | awk '$0 ~ "^+" {count++} END {print count}')
+    if [ $ALL_COMMITS != $TAG_COMMITS ]; then
+        echo -e "${WARN_COL}!!!WARNING!!! Tag $TAG_TO maybe incorrect"
+        echo -e "  Tag commits ($TAG_COMMITS) does not match all commits ($ALL_COMMITS)"
+        echo -e "  Please review using --no-tag option${NO_COL}"
+    fi
+fi
 
-echo -e "${MST_COL}${MST} ${DEV_COL}${DEV}${NO_COL}"
+if [ $COMMIT_DATE -eq 1 ]; then
+    CDATE="${DATE_COL}date "
+else
+    CDATE=""
+fi
+
+echo -e "${CDATE}${MST_COL}${MST} ${DEV_COL}${DEV}${NO_COL}"
 
 IFS=$'\n'
 SHAS=""
 for line in $DIFF; do
+    if [ $COMMIT_DATE -eq 1 ]; then
+        CDATE=$(git log -1 --format="%ci" ${line:2:7})
+        CDATE="${DATE_COL}${CDATE%% *} "
+    fi
     if [[ $line =~ ^\+ ]]; then
-        echo -e "        ${DEV_COL}${line:2}${NO_COL}"
+        echo -e "${CDATE}        ${DEV_COL}${line:2}${NO_COL}"
         SHAS="$SHAS ${line:2:7}"
     elif [[ $line =~ ^- ]]; then
-        echo -e "${MST_COL}${line:2:7}        ${line:9}${NO_COL}"
+        echo -e "${CDATE}${MST_COL}${line:2:7}        ${line:9}${NO_COL}"
     fi
 done
 if [ "$SHAS" != "" ]; then
