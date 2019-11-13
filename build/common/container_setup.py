149879022b483b3e5d051068c8e9d7a12ef4154e
Jonathan Haigh <jonathan.haigh@arm.com>
2019-11-13 13:15:28 +0000

    MBL build: set up SSH and Git whenever we use a build container (#359)
    We have two build scripts that use an MBL build container: build.sh and
    build-update-payloads.sh. We only run ssh-setup.sh and git-setup.sh in
    build.sh, but we should be setting up the container consistently for
    both. 
    
    Add a library for container setup that all build scripts can use, and add a
    "--repo-host" option to build-update-payloads.py so that, like build.sh,
    it can support builds that need SSH access to non-github repos.
    
    For IOTMBL-2619: Bitbake errors when re-building to create update
    payloads.

---
+++ build/common/container_setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Tools for initializing build containers."""
+
+import argparse
+import pathlib
+import subprocess
+import sys
+
+SCRIPTS_DIR = pathlib.Path(__file__).resolve().parent
+
+
+def set_up_container(extra_ssh_hosts=[]):
+    """Initialize a build container."""
+    set_up_git()
+    set_up_ssh(extra_ssh_hosts)
+
+
+def set_up_git():
+    """Initialize a sane git setup."""
+    subprocess.run([str(SCRIPTS_DIR / "git-setup.sh")], check=True)
+
+
+def set_up_ssh(extra_ssh_hosts=[]):
+    """Initialize a sane SSH setup."""
+    subprocess.run(
+        [str(SCRIPTS_DIR / "ssh-setup.sh")] + extra_ssh_hosts, check=True
+    )
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--repo-host",
+        dest="extra_ssh_hosts",
+        metavar="HOST",
+        action="append",
+        help=(
+            "Add a trusted git repository host to the build environment."
+            " Can be specified multiple times."
+        ),
+        default=[],
+    )
+    args, _ = parser.parse_known_args()
+    return args
+
+
+def main(args):
+    """Script entry point."""
+    set_up_container(extra_ssh_hosts=args.extra_ssh_hosts)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(_parse_args()))
