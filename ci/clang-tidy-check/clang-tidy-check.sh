fff783d6d5567073df3aafa620cf09359b93717a
Rob Walton <rob.walton@arm.com>
2020-01-08 09:33:25 +0000

    ci: Add clang-tidy checker. (#403)
    We need a way to run static analysis on our C++ code.
    
    This patch adds a set of shell scripts to perform clang-tidy checks on
    CMake projects in a docker container.
    
    Included:
    
    * clang-tidy-check.sh: shell script to run clang-tidy from inside the
      container
    * Dockerfile: the container specification, installs project dependencies
      for the mbl-core/updated project.
    * run-me.sh: the entry point, runs the dockerr container and executes
      clang-tidy-check.sh
    
    NOTE: to run clang-tidy checks on other CMake projects, any library
    dependencies need to be added to the ci/clang-tidy-check/Dockerfile.
    
    Jira: IOTMBL-2664

---
+++ ci/clang-tidy-check/clang-tidy-check.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Copyright (c) 2020 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+set -e
+set -u
+set -o pipefail
+
+rc=0
+
+find_cmake_projects() {
+    find "$workdir" -name CMakeLists.txt -print0 | xargs -0 --no-run-if-empty grep -l 'option(RUN_CODE_CHECKS ' | xargs --no-run-if-empty dirname
+}
+
+usage()
+{
+  cat <<EOF
+
+usage: clang-tidy-check.sh [OPTION] -- [clang-tidy-check.sh arguments]
+
+  -h, --help            Print brief usage information and exit.
+  --workdir PATH        Specify the directory to check. Default PWD.
+  -x                    Enable shell debugging in this script.
+
+EOF
+}
+
+args=$(getopt -o+hx -l help,workdir: -n "$(basename "$0")" -- "$@")
+eval set -- "$args"
+while [ $# -gt 0 ]; do
+  if [ -n "${opt_prev:-}" ]; then
+    eval "$opt_prev=\$1"
+    opt_prev=
+    shift 1
+    continue
+  elif [ -n "${opt_append:-}" ]; then
+    eval "$opt_append=\"\${$opt_append:-} \$1\""
+    opt_append=
+    shift 1
+    continue
+  fi
+  case $1 in
+  -h | --help)
+    usage
+    exit 0
+    ;;
+
+  --workdir)
+    opt_prev=workdir
+    ;;
+
+  -x)
+    set -x
+    ;;
+
+  --)
+    shift
+    break 2
+    ;;
+  esac
+  shift 1
+done
+
+if [ -z "${workdir:-}" ]; then
+  workdir="$(pwd)"
+fi
+
+workdir=$(readlink -f "$workdir")
+
+CMAKE_PROJECTS=$(printf "%s" "$(find_cmake_projects)")
+if [ -z "$CMAKE_PROJECTS" ]; then
+    printf "No CMake projects configured for clang-tidy found. Exiting.\n"
+    exit 1
+fi
+
+for project in $CMAKE_PROJECTS; do
+    printf "Building \"%s\" with clang-tidy checks enabled.\n" "$project"
+    PROJECT_TMPDIR=/tmp/$(basename "$project")
+    mkdir -p "$PROJECT_TMPDIR"
+    cmake "$project" \
+        --no-warn-unused-cli \
+        -B"$PROJECT_TMPDIR" \
+        -DRUN_CODE_CHECKS=ON \
+        -DCMAKE_CXX_COMPILER=clang \
+        -DCMAKE_INSTALL_LIBDIR=/usr/lib \
+        -DCMAKE_INSTALL_BINDIR=/usr/bin \
+        -S"$project" || rc=1
+    make -C "$PROJECT_TMPDIR" || rc=1
+done
+
+exit $rc
---
+++ ci/clang-tidy-check/run-me.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# Copyright (c) 2020 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+set -e
+set -u
+
+execdir="$(readlink -e "$(dirname "$0")")"
+
+default_imagename="clang-tidy-check-env"
+default_containername="clang-tidy-check-container.$$"
+
+trap cleanup 0
+
+cleanup() {
+    # This command will return an id (eg. 43008e2a9f5a) of the running
+    # container
+    running_container="$(docker ps -q -f name="$default_containername")"
+    if [ ! -z "$running_container" ]; then
+        docker kill "$default_containername"
+    fi
+}
+
+usage()
+{
+  cat <<EOF
+
+usage: run-me.sh [OPTION] -- [run-me.sh arguments]
+
+  -h, --help            Print brief usage information and exit.
+  --image-name NAME     Specify the docker image name to build. Default ${default_imagename}.
+  --tty                 Enable tty creation (default).
+  --no-tty              Disable tty creation.
+  --workdir PATH        Specify the directory to check.  Default PWD.
+  -x                    Enable shell debugging in this script.
+
+EOF
+}
+
+imagename="$default_imagename"
+flag_tty="-t"
+
+args=$(getopt -o+hx -l help,image-name:,tty,no-tty,workdir: -n "$(basename "$0")" -- "$@")
+eval set -- "$args"
+while [ $# -gt 0 ]; do
+  if [ -n "${opt_prev:-}" ]; then
+    eval "$opt_prev=\$1"
+    opt_prev=
+    shift 1
+    continue
+  elif [ -n "${opt_append:-}" ]; then
+    eval "$opt_append=\"\${$opt_append:-} \$1\""
+    opt_append=
+    shift 1
+    continue
+  fi
+  case $1 in
+  -h | --help)
+    usage
+    exit 0
+    ;;
+
+  --image-name)
+    opt_prev=imagename
+    ;;
+
+  --tty)
+    flag_tty="-t"
+    ;;
+
+  --no-tty)
+    flag_tty=
+    ;;
+
+  --workdir)
+    opt_prev=workdir
+    ;;
+
+  -x)
+    set -x
+    ;;
+
+  --)
+    shift
+    break 2
+    ;;
+  esac
+  shift 1
+done
+
+if [ -z "${workdir:-}" ]; then
+  workdir="$(pwd)"
+fi
+workdir=$(readlink -f "$workdir")
+
+docker build -t "$imagename" "$execdir"
+
+docker run --rm -i $flag_tty \
+       --name "$default_containername" \
+       -e LOCAL_UID="$(id -u)" -e LOCAL_GID="$(id -g)" \
+       -v "$workdir":/work "$imagename" \
+       ./clang-tidy-check.sh --workdir /work \
+       "$@"
